7e4e7633edf87920e2b16af9affb7681cad188c7
Nikita-stud <nikita151998de@gmail.com>
2025-07-10 11:58:31 +0200

    Register Page almost all HTML and CSS for the Mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ["EB Garamond"],
+        // helvetica: ["EB Garamond"],
+      },
+      fontSize: {
+        mobileMainHeader: "36px",
+        mobileSecondaryHeader: "28px",
+        mobileButton: "21px",
+        mobileText: "16px",
+      },
+      colors: {
+        white: "#F3FFFB",
+        formWhite: "#FFFFFF",
+        dark: "#101802",
+        grey: "#757575",
+        yellow: "#FFCA13",
+        darkerYellow: "#92613A",
+      },
+    },
   },
   plugins: [],
 };
