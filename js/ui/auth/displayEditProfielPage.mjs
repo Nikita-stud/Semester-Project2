aab10590119f3a7a95b42b1993250b33cc8128c4
Nikita-stud <nikita151998de@gmail.com>
2025-09-10 20:49:05 +0200

    disable button on no values in edit page

--- js/ui/auth/displayEditProfielPage.mjs
+++ js/ui/auth/displayEditProfielPage.mjs
@@ -1,7 +1,9 @@
 const avatarImg = document.getElementById("profilePageImage");
 const bannerImg = document.getElementById("profilePageBackground");
+const form = document.getElementById("editProfileForm");
 const name = document.getElementById("name");
 const email = document.getElementById("email");
+const saveCta = document.getElementById("saveProfile");
 const cancelCta = document.getElementById("cancel");
 const bio = document.getElementById("bio");
 
@@ -26,6 +28,19 @@ export function displayEditProfilePage(profile) {
   email.placeholder = `${profile.email}`;
   const bioText = profile.bio || "Write something about yourself";
   bio.placeholder = `${bioText}`;
+
+  form.addEventListener("input", () => {
+    const data = new FormData(form);
+    const values = Object.fromEntries(data);
+    const { banner, avatar, bio } = values;
+    if (banner || avatar || bio) {
+      saveCta.disabled = false;
+      saveCta.classList.remove("opacity-50");
+    } else {
+      saveCta.disabled = true;
+      saveCta.classList.add("opacity-50");
+    }
+  });
 
   cancelCta.addEventListener("click", () => {
     window.location.replace(`/profile/index.html`);
